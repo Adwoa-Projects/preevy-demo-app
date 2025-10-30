1a5405eb6be34b54e330ee6548cc297e85eedf4f
Thelma Boamah <thelma.a.boamah@gmail.com>
2025-10-30 07:01:11 -0400

    multiply

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(10);
+  const [count, setCount] = useState(1);
 
   return (
     <>
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Preevy Demo App</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 10)}>
+        <button onClick={() => setCount((count) => count * 5)}>
           count is {count}
         </button>
         <p>
